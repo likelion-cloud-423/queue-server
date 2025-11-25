12c3898eb0b9d22e3e13184ea29b5a85bba8e0b1
pid011 <pid011@outlook.com>
2025-11-26 07:45:13 +0900

    Init project

---
+++ queue-manager/src/test/java/com/likelion/queuemanager/QueueManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.likelion.queuemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
