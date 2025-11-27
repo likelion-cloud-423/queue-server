96885904b2f93eccd8bcf6df02e294fac2eabfe5
pid011 <pid011@outlook.com>
2025-11-27 10:52:15 +0900

    Ignore queue directory and untrack files

--- queue/queue-api/src/main/java/com/likelion/queueapi/QueueController.java
+++
@@ -1,9 +0,0 @@
-package com.likelion.queueapi;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/queue")
-public class QueueController {
-}
