ad68eeed4aa18bb76434412c3e48cccebc817318
pid011 <pid011@outlook.com>
2025-12-03 06:06:13 +0900

    otel logging 설정

---
+++ queue/queue-api/src/main/java/com/likelion/queueapi/config/OpenTelemetryConfig.java
@@ -0,0 +1,20 @@
+package com.likelion.queueapi.config;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenTelemetryConfig {
+
+    @Bean
+    public OpenTelemetry openTelemetry() {
+        OpenTelemetry openTelemetry = AutoConfiguredOpenTelemetrySdk.initialize()
+                .getOpenTelemetrySdk();
+        OpenTelemetryAppender.install(openTelemetry);
+        return openTelemetry;
+    }
+}
